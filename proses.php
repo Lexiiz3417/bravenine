4e6849ff90376739e2dcdd4ed162d814c61a1dc0
Lexiiz <48111598+Lexiiz3417@users.noreply.github.com>
2023-12-10 14:54:07 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -13,3 +13,16 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Halaman Terkunci</title>
+</head>
+<body>
+    <h2>Selamat datang di Halaman Terkunci!</h2>
+    <!-- Konten halaman terkunci -->
+</body>
+</html>
