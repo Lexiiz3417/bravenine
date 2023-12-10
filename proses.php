a1c3c6ef539962e14aedba66cc22762541b43fe6
Lexiiz <48111598+Lexiiz3417@users.noreply.github.com>
2023-12-10 14:55:51 +0700

    Update proses.php

--- proses.php
+++ proses.php
@@ -3,17 +3,7 @@ $allowed_password = "sandi123";
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
     $input_password = isset($_POST["password"]) ? $_POST["password"] : "";
-
-    if ($input_password === $allowed_password) {
-        header("Location: halaman_terkunci.html");
-        exit();
-    } else {
-        header("Location: index.html");
-        exit();
-    }
-}
-?>
-
+        
 <!DOCTYPE html>
 <html lang="en">
 <head>
